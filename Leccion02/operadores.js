a0f2afada7902da8d5acf248c00bf9986cd6b467
DayannaR16 <alexamontenegro168@gmail.com>
2022-08-29 08:39:16 -0500

    leccion 2 y 3

---
+++ Leccion02/operadores.js
@@ -0,0 +1,50 @@
+//Operador suma
+let a = 3, b=2;
+let z= a + b;
+console.log ("Resultado de la suma: " + z);
+
+//Operador resta
+z= a - b ;
+console.log ("Resultado de la resta: " + z);
+
+//Multiplicacion 
+z= a * b;
+console.log ("Resultado de la multiplicacion es: "+z);
+
+//Division
+z= a / b;
+console.log ("Resultado de la division: "+ z);
+
+//Operacion de modulo
+z= a % b;
+console.log ("Residuo de la division es: "+ z);
+
+//operador de exponente 
+z= a ** b;
+console.log ("Resultado de operador exponente: " + z);
+
+//Operacdores de incremento y decremento
+//Incremeto
+//Pre-incremento(el operador ++ antes de la variable)
+z= ++a;
+console.log (a);
+console.log(z);
+
+//post-incremento(el operador ++ despues de la variable)
+z = b++;
+console.log(b);
+console.log(z);
+
+//Pre-Decremento (Primero el operador de --)
+z= --a;
+console.log(a);
+console.log(z);
+
+//post-Decremento
+z= b--;
+console.log(b);
+console.log(z);
+
+
+
+
